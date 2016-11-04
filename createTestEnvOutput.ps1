d274151c1ce3173a0494f51951cd21237ec97101
NNajran <narjit.najran@gmail.com>
2016-11-04 02:00:49 +0000

    Add files via upload

---
+++ createTestEnvOutput.ps1
@@ -0,0 +1,139 @@
+info:    Executing command group deployment create
+info:    Supply values for the following parameters
+administratorLoginPassword:  <Enter password>
++ Initializing template configurations and parameters
++ Creating a deployment
+info:    Created template deployment "DeployTestEnv1"
++ Waiting for deployment to complete
++
++
++
++
+info:    Resource 'pr-appserviceplan1' of type 'Microsoft.Web/serverfarms' provisioning status is Succeeded
++
+info:    Resource 'pr-webapp-test1' of type 'Microsoft.Web/sites' provisioning status is Succeeded
+info:    Resource 'pr-apiapp-test1' of type 'Microsoft.Web/sites' provisioning status is Succeeded
+info:    Resource 'pr-appserviceplan1' of type 'Microsoft.Web/serverfarms' provisioning status is Succeeded
++
+info:    Resource 'pr-webapp-test1' of type 'Microsoft.Web/sites' provisioning status is Succeeded
+info:    Resource 'pr-apiapp-test1' of type 'Microsoft.Web/sites' provisioning status is Succeeded
+info:    Resource 'pr-appserviceplan1' of type 'Microsoft.Web/serverfarms' provisioning status is Succeeded
++
+info:    Resource 'pr-webapp-test1' of type 'Microsoft.Web/sites' provisioning status is Succeeded
+info:    Resource 'pr-apiapp-test1' of type 'Microsoft.Web/sites' provisioning status is Succeeded
+info:    Resource 'pr-appserviceplan1' of type 'Microsoft.Web/serverfarms' provisioning status is Succeeded
++
+info:    Resource 'pr-dbserver1-test1/AllowAllWindowsAzureIps' of type 'Microsoft.Sql/servers/firewallrules' provisionin
+g status is Succeeded
+info:    Resource 'pr-dbserver1-test1/PR_DB_TEST1' of type 'Microsoft.Sql/servers/databases' provisioning status is Runn
+ing
+info:    Resource 'pr-dbserver1-test1' of type 'Microsoft.Sql/servers' provisioning status is Succeeded
+info:    Resource 'pr-webapp-test1' of type 'Microsoft.Web/sites' provisioning status is Succeeded
+info:    Resource 'pr-apiapp-test1' of type 'Microsoft.Web/sites' provisioning status is Succeeded
+info:    Resource 'pr-appserviceplan1' of type 'Microsoft.Web/serverfarms' provisioning status is Succeeded
++
+info:    Resource 'pr-dbserver1-test1/AllowAllWindowsAzureIps' of type 'Microsoft.Sql/servers/firewallrules' provisionin
+g status is Succeeded
+info:    Resource 'pr-dbserver1-test1/PR_DB_TEST1' of type 'Microsoft.Sql/servers/databases' provisioning status is Runn
+ing
+info:    Resource 'pr-dbserver1-test1' of type 'Microsoft.Sql/servers' provisioning status is Succeeded
+info:    Resource 'pr-webapp-test1' of type 'Microsoft.Web/sites' provisioning status is Succeeded
+info:    Resource 'pr-apiapp-test1' of type 'Microsoft.Web/sites' provisioning status is Succeeded
+info:    Resource 'pr-appserviceplan1' of type 'Microsoft.Web/serverfarms' provisioning status is Succeeded
++
+info:    Resource 'pr-dbserver1-test1/AllowAllWindowsAzureIps' of type 'Microsoft.Sql/servers/firewallrules' provisionin
+g status is Succeeded
+info:    Resource 'pr-dbserver1-test1/PR_DB_TEST1' of type 'Microsoft.Sql/servers/databases' provisioning status is Runn
+ing
+info:    Resource 'pr-dbserver1-test1' of type 'Microsoft.Sql/servers' provisioning status is Succeeded
+info:    Resource 'pr-webapp-test1' of type 'Microsoft.Web/sites' provisioning status is Succeeded
+info:    Resource 'pr-apiapp-test1' of type 'Microsoft.Web/sites' provisioning status is Succeeded
+info:    Resource 'pr-appserviceplan1' of type 'Microsoft.Web/serverfarms' provisioning status is Succeeded
++
+info:    Resource 'pr-dbserver1-test1/AllowAllWindowsAzureIps' of type 'Microsoft.Sql/servers/firewallrules' provisionin
+g status is Succeeded
+info:    Resource 'pr-dbserver1-test1/PR_DB_TEST1' of type 'Microsoft.Sql/servers/databases' provisioning status is Runn
+ing
+info:    Resource 'pr-dbserver1-test1' of type 'Microsoft.Sql/servers' provisioning status is Succeeded
+info:    Resource 'pr-webapp-test1' of type 'Microsoft.Web/sites' provisioning status is Succeeded
+info:    Resource 'pr-apiapp-test1' of type 'Microsoft.Web/sites' provisioning status is Succeeded
+info:    Resource 'pr-appserviceplan1' of type 'Microsoft.Web/serverfarms' provisioning status is Succeeded
++
+info:    Resource 'pr-dbserver1-test1/AllowAllWindowsAzureIps' of type 'Microsoft.Sql/servers/firewallrules' provisionin
+g status is Succeeded
+info:    Resource 'pr-dbserver1-test1/PR_DB_TEST1' of type 'Microsoft.Sql/servers/databases' provisioning status is Runn
+ing
+info:    Resource 'pr-dbserver1-test1' of type 'Microsoft.Sql/servers' provisioning status is Succeeded
+info:    Resource 'pr-webapp-test1' of type 'Microsoft.Web/sites' provisioning status is Succeeded
+info:    Resource 'pr-apiapp-test1' of type 'Microsoft.Web/sites' provisioning status is Succeeded
+info:    Resource 'pr-appserviceplan1' of type 'Microsoft.Web/serverfarms' provisioning status is Succeeded
++
+info:    Resource 'pr-dbserver1-test1/AllowAllWindowsAzureIps' of type 'Microsoft.Sql/servers/firewallrules' provisionin
+g status is Succeeded
+info:    Resource 'pr-dbserver1-test1/PR_DB_TEST1' of type 'Microsoft.Sql/servers/databases' provisioning status is Runn
+ing
+info:    Resource 'pr-dbserver1-test1' of type 'Microsoft.Sql/servers' provisioning status is Succeeded
+info:    Resource 'pr-webapp-test1' of type 'Microsoft.Web/sites' provisioning status is Succeeded
+info:    Resource 'pr-apiapp-test1' of type 'Microsoft.Web/sites' provisioning status is Succeeded
+info:    Resource 'pr-appserviceplan1' of type 'Microsoft.Web/serverfarms' provisioning status is Succeeded
++
+info:    Resource 'pr-dbserver1-test1/AllowAllWindowsAzureIps' of type 'Microsoft.Sql/servers/firewallrules' provisionin
+g status is Succeeded
+info:    Resource 'pr-dbserver1-test1/PR_DB_TEST1' of type 'Microsoft.Sql/servers/databases' provisioning status is Runn
+ing
+info:    Resource 'pr-dbserver1-test1' of type 'Microsoft.Sql/servers' provisioning status is Succeeded
+info:    Resource 'pr-webapp-test1' of type 'Microsoft.Web/sites' provisioning status is Succeeded
+info:    Resource 'pr-apiapp-test1' of type 'Microsoft.Web/sites' provisioning status is Succeeded
+info:    Resource 'pr-appserviceplan1' of type 'Microsoft.Web/serverfarms' provisioning status is Succeeded
++
+info:    Resource 'diagnosticsSettings_v5l2cai6ppxhw' of type 'Microsoft.Resources/deployments' provisioning status is R
+unning
+info:    Resource 'pr-dbserver1-test1/AllowAllWindowsAzureIps' of type 'Microsoft.Sql/servers/firewallrules' provisionin
+g status is Succeeded
+info:    Resource 'pr-dbserver1-test1/PR_DB_TEST1' of type 'Microsoft.Sql/servers/databases' provisioning status is Succ
+eeded
+info:    Resource 'pr-dbserver1-test1' of type 'Microsoft.Sql/servers' provisioning status is Succeeded
+info:    Resource 'pr-webapp-test1' of type 'Microsoft.Web/sites' provisioning status is Succeeded
+info:    Resource 'pr-apiapp-test1' of type 'Microsoft.Web/sites' provisioning status is Succeeded
+info:    Resource 'pr-appserviceplan1' of type 'Microsoft.Web/serverfarms' provisioning status is Succeeded
++
+info:    Resource 'diagnosticsSettings_v5l2cai6ppxhw' of type 'Microsoft.Resources/deployments' provisioning status is S
+ucceeded
+info:    Resource 'pr-dbserver1-test1/AllowAllWindowsAzureIps' of type 'Microsoft.Sql/servers/firewallrules' provisionin
+g status is Succeeded
+info:    Resource 'pr-dbserver1-test1/PR_DB_TEST1' of type 'Microsoft.Sql/servers/databases' provisioning status is Succ
+eeded
+info:    Resource 'pr-dbserver1-test1' of type 'Microsoft.Sql/servers' provisioning status is Succeeded
+info:    Resource 'pr-webapp-test1' of type 'Microsoft.Web/sites' provisioning status is Succeeded
+info:    Resource 'pr-apiapp-test1' of type 'Microsoft.Web/sites' provisioning status is Succeeded
+info:    Resource 'pr-appserviceplan1' of type 'Microsoft.Web/serverfarms' provisioning status is Succeeded
+data:    DeploymentName     : DeployTestEnv1
+data:    ResourceGroupName  : PR-RG1
+data:    ProvisioningState  : Succeeded
+data:    Timestamp          : Fri Nov 04 2016 00:13:50 GMT+0000 (Coordinated Universal Time)
+data:    Mode               : Incremental
+data:    CorrelationId      : 69c1d1cc-558c-44bd-bff5-402580cca4f1
+data:    DeploymentParameters :
+data:    Name                         Type          Value
+data:    ---------------------------  ------------  ------------------------------------
+data:    name                         String        pr-webapp-test1
+data:    api_app_name                 String        pr-apiapp-test1
+data:    hostingPlanName              String        pr-appserviceplan1
+data:    location                     String        UK West
+data:    sku                          String        Basic
+data:    skuCode                      String        B1
+data:    workerSize                   String        0
+data:    serverFarmResourceGroup      String        PR-RG1
+data:    subscriptionId               String        bd1e690d-19f3-4bc5-8e9e-0543809bdf40
+data:    administratorLogin           String        prdba1test1
+data:    administratorLoginPassword   SecureString  undefined
+data:    collation                    String        SQL_Latin1_General_CP1_CI_AS
+data:    databaseName                 String        PR_DB_TEST1
+data:    edition                      String        Basic
+data:    requestedServiceObjectiveId  String        dd6d99bb-f193-4ec1-86f2-43d3bccbc49c
+data:    maxSizeBytes                 String        2147483648
+data:    serverName                   String        pr-dbserver1-test1
+data:    version                      String        12.0
+data:    diagnosticStorageAccountId   String        SA1
+data:    diagnosticsTemplate          String        DiagnosticsWithNoStorageAccount.json
+info:    group deployment create command OK
